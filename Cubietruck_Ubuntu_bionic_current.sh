2607784624a9ad1a31fcd64c87c59e2d2a8aa875
Stephan Linz <linz@li-pro.net>
2019-09-30 07:12:54 +0200

    buildenv: build script for Cubietruck
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ Cubietruck_Ubuntu_bionic_current.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (c) 2016-2019 Stephan Linz, linz@li-pro.net
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+# This file is a part of tool chain https://github.com/lipro-armbian/build
+#
+# Refer to:
+#   https://docs.armbian.com/#supported-chips
+#   https://docs.armbian.com/Hardware_Allwinner-A20/#allwinner-a10-a20-boards
+#   https://docs.armbian.com/boards/cubietruck/
+#   https://www.armbian.com/cubietruck/
+#   http://linux-sunxi.org/Cubietruck
+#   http://dl.linux-sunxi.org/
+#   http://cubieboard.org/model/cb3/
+#   http://docs.cubieboard.org/tutorials/cubietruck/start
+#   http://dl.cubieboard.org/
+#
+# Additional useful options:
+#   DEFAULT_OVERLAYS="cubietruck-blue-heartbeat cubietruck-orange-stmmac-link cubietruck-white-disk-activity cubietruck-green-mmc0"
+# ----------------------------------------------------------------------------
+export LANG=""
+exec ./compile.sh docker \
+	BOARD=cubietruck \
+	BRANCH=current \
+	RELEASE=bionic \
+	BUILD_DESKTOP=no \
+	KERNEL_ONLY=no \
+	KERNEL_CONFIGURE=no
