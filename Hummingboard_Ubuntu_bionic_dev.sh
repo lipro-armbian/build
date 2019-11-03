42017de860d0d488ac47e58f5a072abe847f2626
Stephan Linz <linz@li-pro.net>
2019-11-03 07:51:10 +0100

    buildenv: build script for HummingBoard
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ Hummingboard_Ubuntu_bionic_dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) 2017-2019 Stephan Linz, linz@li-pro.net
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+# This file is a part of tool chain https://github.com/lipro-armbian/build
+#
+# Refer to:
+#   https://docs.armbian.com/#supported-chips
+#   https://docs.armbian.com/Hardware_Freescale-imx6/#cubox-and-hummingboard-boards
+#   https://www.armbian.com/hummingboard/
+#   https://developer.solid-run.com/products/imx6-som/
+#   https://developer.solid-run.com/products/hummingboard-gate-edge/
+#
+# ----------------------------------------------------------------------------
+export LANG=""
+exec ./compile.sh docker \
+	BOARD=hummingboard \
+	BRANCH=dev \
+	RELEASE=bionic \
+	BUILD_DESKTOP=no \
+	KERNEL_ONLY=no \
+	KERNEL_CONFIGURE=no
